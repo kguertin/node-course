72ac0d0fe261602f576140fa17df9fe75f5a85c7
Kevin <kevin.m.guertin@gmail.com>
2020-05-04 17:17:40 -0400

    refactor code to use writeFile and reduce blocking code

--- node-basics/app.js
+++ node-basics/app.js
@@ -21,14 +21,14 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.writeHead(302, { 'Location': '/' })
+        return res.end()
+      });
     });
-
-    res.writeHead(302, { 'Location': '/' })
-    return res.end()
   }
 
   res.setHeader('Content-Type', 'text/html')
