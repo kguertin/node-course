f5a1dac6da64a5921882766f09a8d816b7b816a2
Kevin <kevin.m.guertin@gmail.com>
2020-05-04 16:35:18 -0400

    make post request to server and write dummy text to file. redirect user to root

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,10 +1,25 @@
 // Node server w/o Express
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit()
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'Dummy');
+    res.writeHead(302, { 'Location': '/' })
+    return res.end()
+  }
+
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
   res.write('<head><title>My First Page</title></head>')
