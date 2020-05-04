1c715231052f13daf67b3e5ea95070f6519cc01f
Kevin <kevin.m.guertin@gmail.com>
2020-05-04 16:58:40 -0400

    parsed data using chunks and buffer

--- node-basics/app.js
+++ node-basics/app.js
@@ -15,7 +15,18 @@ const server = http.createServer((req, res) => {
     return res.end()
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'Dummy');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
     res.writeHead(302, { 'Location': '/' })
     return res.end()
   }
