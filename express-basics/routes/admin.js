bdae8b75914e4e0bc5d87b332cb4b44a826703c8
Kevin <kevin.m.guertin@gmail.com>
2020-05-05 17:21:43 -0400

    add styling and public folder to serve it staticly

--- express-basics/routes/admin.js
+++ express-basics/routes/admin.js
@@ -2,15 +2,17 @@ const path = require('path')
 
 const express = require('express');
 
-const router = express.Router()
+const rootDir = require('../util/path');
+
+const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
-})
+});
 
 module.exports = router
