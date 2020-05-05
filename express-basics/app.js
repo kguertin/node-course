d837319d865787107be39941df8c6c4f2592a36d
Kevin <kevin.m.guertin@gmail.com>
2020-05-05 12:40:25 -0400

    basic express route handling

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,18 +1,19 @@
-const http = require('http');
-
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middlewear!');
+app.use('/', (req, res, next) => {
+  console.log('This Always Runs!');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middlewear!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middlewear!');
   res.send('<h1>Hello From Express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
