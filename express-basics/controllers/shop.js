c298205aba45550077948f7f7b6ca1fed66f4144
Kevin <kevin.m.guertin@gmail.com>
2020-05-11 17:07:46 -0400

    post id to cart and created ejs partial to render add to cart button

--- express-basics/controllers/shop.js
+++ express-basics/controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res) => {
   })
 }
 
+exports.postCart = (req, res) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+}
+
 exports.getOrders = (req, res) => {
   res.render('./shop/orders', {
     path: '/orders',
