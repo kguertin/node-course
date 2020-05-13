4cd40bfcda7ce1cdcd18c0a52dfbbc531deab5d0
Kevin <kevin.m.guertin@gmail.com>
2020-05-13 17:02:23 -0400

    imstall mysql and connect database to server

--- express-basics/app.js
+++ express-basics/app.js
@@ -5,13 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
