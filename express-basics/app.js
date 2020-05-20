23cced7a9d0946f96deef4020c6c54e40cf4e433
Kevin <kevin.m.guertin@gmail.com>
2020-05-20 17:12:21 -0400

    created user and added to request object through middlewear

--- express-basics/app.js
+++ express-basics/app.js
@@ -17,9 +17,17 @@ const shopRoutes = require('./routes/shop');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -29,8 +37,13 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
-sequelize.sync({ force: true })
-  .then(res => {
-    // console.log(res)
-    app.listen(3000);
-  }).catch(err => console.log(err));
+sequelize.sync()
+  .then(() => User.findByPk(1))
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Kevin', email: 'test@test.com' });
+    }
+    return user
+  })
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err));
