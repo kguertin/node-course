d8e3651972cb7a04f0f96bacfa35cb21867e90e7
Kevin <kevin.m.guertin@gmail.com>
2020-05-20 19:35:32 -0400

    refactor cart model using sequrlize and made bridge table for the cart and product

--- express-basics/app.js
+++ express-basics/app.js
@@ -10,6 +10,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -36,8 +38,12 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync()
+sequelize.sync({ force: true })
   .then(() => User.findByPk(1))
   .then(user => {
     if (!user) {
