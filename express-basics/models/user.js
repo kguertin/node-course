90057117d1b7434d4ea0438fc333748332bb89b8
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-26 20:17:52 -0400

    add cart embeded into user model, still hav to add items and populte cart view

--- express-basics/models/user.js
+++ express-basics/models/user.js
@@ -2,9 +2,11 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -15,6 +17,26 @@ class User {
       .catch(err => console.log(err))
   }
 
+  addToCart(product) {
+    //     const cartProduct = this.cart.items.findIndex(cp => {
+    //       return cp._id === product._id;
+    //     })
+    const updatedCart = {
+      items: [{
+        ...product,
+        quantity: 1
+      }]
+    };
+    const db = getDb();
+    db.collection('users')
+      .updateOne({
+        _id: new mongodb.ObjectId(this._id)
+      }, { $set: {cart: updatedCart}}
+                )
+      .then()
+      .catch(err => console.log(err))
+  }
+
   static findById(userId) {
     const db = getDb();
     return db.collection('users')
@@ -22,9 +44,9 @@ class User {
         _id: new mongodb.ObjectId(userId)
       })
       .then(user => {
-      console.log('user sent: ', user);
-      return user;
-    })
+        console.log('user sent: ', user);
+        return user;
+      })
       .catch(err => console.log(err));
   }
 }
