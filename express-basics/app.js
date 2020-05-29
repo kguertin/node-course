e47eb52874ee6ee0331441332ce990bbe2fed062
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-29 17:30:42 -0400

    add user schema and model

--- express-basics/app.js
+++ express-basics/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -20,14 +20,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5ecda678612453e0013669b9')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id); // Otherwise doesnt have access to methods on user model 
-//       next();
-//     })
-//     .catch(err => console.log(err))
-// })
+app.use((req, res, next) => {
+  User.findById('5ed17e8b0418670616aeb06e')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,5 +35,20 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://kevin:node1234@cluster0-kmmuu.mongodb.net/shop?retryWrites=true&w=majority')
-  .then(() => app.listen(3000))
+  .then(() => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Kevin',
+            email: 'test@test.com',
+            cart: {
+              items: []
+            }
+          })
+          user.save()
+        }
+      })
+    app.listen(3000)
+  })
   .catch(err => console.log(err));
