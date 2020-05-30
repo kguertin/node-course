9103910dde1bc93c6ff6ad1d1237003b4bb26514
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-30 15:18:38 -0400

    setup login view route and controller

--- express-basics/app.js
+++ express-basics/app.js
@@ -11,6 +11,7 @@ const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
