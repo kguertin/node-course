1f7e9db0a706cedf07977f4435f4102ef3019c9b
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-31 18:55:49 -0400

    add session and used to mark user as logged in

--- express-basics/app.js
+++ express-basics/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const app = express();
 
@@ -20,6 +21,11 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use((req, res, next) => {
   User.findById('5ed17e8b0418670616aeb06e')
--- express-basics/controllers/auth.js
+++ express-basics/controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res) => {
-  const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1];
+//   const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn)
   res.render('auth/login',{
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   })
 }
 
 exports.postLogin= (req, res) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  req.session.isLoggedIn = true
   res.redirect('/');
 }
