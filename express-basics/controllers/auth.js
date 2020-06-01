b8240dde77a0010ed7b77c4c1275147dc7beefa1
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-05-31 21:46:10 -0400

    can destroy cookie on logout

--- express-basics/controllers/auth.js
+++ express-basics/controllers/auth.js
@@ -19,3 +19,10 @@ exports.postLogin = (req, res) => {
     })
     .catch(err => console.log(err));
 }
+
+exports.postLogout = (req, res) => {
+  req.session.destroy(err =>{
+    console.log(err);
+    res.redirect('/');
+  });
+}
