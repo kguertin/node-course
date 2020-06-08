85b42b1dde405a03ed460ec2de91145894c8dc88
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-06-08 14:23:59 -0400

    add validation to edit products route

--- express-basics/controllers/admin.js
+++ express-basics/controllers/admin.js
@@ -9,7 +9,8 @@ exports.getAddProduct = (req, res) => {
     path: '/admin/add-product',
     editing: false,
     hasError: false,
-    errorMessage: null
+    errorMessage: null,
+    validationErrors: []
   })
 }
 
@@ -32,7 +33,8 @@ exports.postAddProduct = (req, res) => {
         description: description,
       },
       hasError: true,
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
     })
   }
 
@@ -69,7 +71,8 @@ exports.getEditProduct = (req, res) => {
         editing: editMode,
         product: product,
         hasError: false,
-        errorMessage: null
+        errorMessage: null,
+        validationErrors: []
       })
     })
     .catch(err => console.log(err));
@@ -81,6 +84,25 @@ exports.postEditProduct = (req, res) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('./admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDescription,
+        _id: prodId
+      },
+      hasError: true,
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    })
+  }
 
   Product.findById(prodId)
     .then(product => {
