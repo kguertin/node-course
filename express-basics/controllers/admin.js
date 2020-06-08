6db8c292459d2b9a237cb88b0330566964429553
kevin.m.guertin <kevin.m.guertin@gmail.com>
2020-06-08 13:11:29 -0400

    add validation to the add products page

--- express-basics/controllers/admin.js
+++ express-basics/controllers/admin.js
@@ -1,10 +1,15 @@
 const Product = require('../models/product');
+const {
+  validationResult
+} = require('express-validator');
 
 exports.getAddProduct = (req, res) => {
   res.render('./admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null
   })
 }
 
@@ -13,6 +18,24 @@ exports.postAddProduct = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('./admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    })
+  }
+
   const product = new Product({
     title,
     price,
@@ -44,7 +67,9 @@ exports.getEditProduct = (req, res) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        hasError: false,
+        errorMessage: null
       })
     })
     .catch(err => console.log(err));
