1f0d9f8389e49cf47d9429b4592d8910e800b679
kguertin <kevin.m.guertin@gmail.com>
2020-06-13 16:03:44 -0400

    first part of auth tutorial completed

---
+++ react-jwt-sessions/second_pass/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// Set Up Mongoose
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) throw err;
+    console.log('Mongodb connection established');
+})
+
+// Set Routes
+app.use('/users', require('./routes/userRouter'));
+
