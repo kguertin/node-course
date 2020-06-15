6a3ead7863a7df2f1b262752d203df9c62ab1275
kguertin <kevin.m.guertin@gmail.com>
2020-06-14 20:57:27 -0400

    add useContext and state to check user login

--- mern-client/src/App.js
+++ mern-client/src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import axios from 'axios'
 import Header from './components/layout/Header';
 import Home from './components/pages/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import UserContext from './context/UserContext'
 
 import './style.css'
 
 export default function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined
+    });
+    
+    useEffect(()=>{
+        const checkedLoggedIn = async () => {
+            let token = localStorage.getItem("auth-token");
+            if (token === null) {
+                localStorage.setItem("auth-token", "");
+                token = '';
+            }
+            const tokenRes = await axios.post('http://localhost:5000/users/tokenIsValid', null, {headers: {
+                "x-auth-token": token
+            }});
+            if (tokenRes.data) {
+                
+            }
+        };
+        checkedLoggedIn()
+    }, []);
+    // Empty array (dependency array) means it only runs on mount not a state change
     return (
         <>
         {/* BrowserRouter gives access to routing for components inside */}
             <BrowserRouter> 
+            <UserContext.Provider value={{userData, setUserData}}>
             <Header />
             {/* Switch gives access to the url */}
                 <Switch>
@@ -19,6 +44,7 @@ export default function App() {
                     <Route path="/Login" component={Login} />
                     <Route path="/Register" component={Register} />
                 </Switch>
+            </UserContext.Provider>
             </BrowserRouter>
         </>
     )
--- react-jwt-sessions/second_pass/index.js
+++ react-jwt-sessions/second_pass/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
