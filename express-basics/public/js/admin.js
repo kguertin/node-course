f41a1c3ea3f23bd1def86864876559704ff7b424
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-21 17:39:49 -0400

    remove product with async js

--- express-basics/public/js/admin.js
+++ express-basics/public/js/admin.js
@@ -2,12 +2,19 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+    const productElement = btn.closest('article');
+
     fetch(`/admin/product/${prodId}`, {
         method: 'DELETE',
         headers: {
             "csrf-token": csrf
         }
     })
-    .then(res => console.log(res))
+    .then(res => res.json)
+    .then(data => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement);
+
+    })
     .catch(err => console.log(err));
 }
