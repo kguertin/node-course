ed0d7b2432357e589354cc6e803cf39558a44252
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-23 14:42:23 -0400

    add static image folder and error handling middleware

--- rest-apis/app.js
+++ rest-apis/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+const { static } = require('express');
 
 const MONGODB_URI = 'mongodb+srv://kevin:node1234@cluster0-kmmuu.mongodb.net/messages';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +22,15 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    })
+})
 
 mongoose.connect(MONGODB_URI)
 .then(() => app.listen(8080))
--- rest-apis/controllers/feed.js
+++ rest-apis/controllers/feed.js
@@ -16,13 +16,12 @@ exports.getPosts = (req, res) => {
     })
 }
 
-exports.createPost = (req, res) => {
+exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(422).json({
-            message: 'validation failed, entered data is incorrect.',
-            errors: errors.array()
-        })
+        const error = new Error('validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
     }
 
     const title = req.body.title;
@@ -43,5 +42,10 @@ exports.createPost = (req, res) => {
                 post: response
             })
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
