0d70cff59627f046f028247d82d49b933b62aefd
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-23 13:35:34 -0400

    store new post data in db

--- rest-apis/controllers/feed.js
+++ rest-apis/controllers/feed.js
@@ -1,5 +1,7 @@
 const {validationResult} = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res) => {
     res.status(200).json({
         posts: [{
@@ -25,17 +27,21 @@ exports.createPost = (req, res) => {
 
     const title = req.body.title;
     const content = req.body.content;
-    //Create post in db
-    res.status(201).json({
-        message: 'Post created successfully',
-        post: {
-            _id: new Date().toISOString(),
-            title,
-            content,
-            creator: {
-                name: 'Kevin'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/garden_of_the_moon.jpg',
+        creator: {
+            name: 'Kevin'
         }
     })
+    post.save()
+        .then(response => {
+            console.log(res);
+            res.status(201).json({
+                message: 'Post created successfully',
+                post: response
+            })
+        })
+        .catch(err => console.log(err));
 }
