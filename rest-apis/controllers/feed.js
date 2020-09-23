f71d4c870275446158fc32b6e5668dac10a40fbe
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-23 12:32:25 -0400

    send post data to react client

--- rest-apis/controllers/feed.js
+++ rest-apis/controllers/feed.js
@@ -1,12 +1,21 @@
 exports.getPosts = (req, res) => {
     res.status(200).json({
-        posts: [{title: 'First Post', content: "This is the first post."}]
+        posts: [{
+            title: 'First Post', 
+            content: "This is the first post.", 
+            imageUrl: 'images/garden_of_the_moon.jpg', 
+            creator:{
+            name: 'Kevin'
+        },
+        createdAt: new Date()
+        }]
     })
 }
 
 exports.createPost = (req, res) => {
     const title = req.body.title;
     const content = req.body.content;
+    console.log(title, content)
     //Create post in db
     res.status(201).json({
         message: 'Post created successfully',
