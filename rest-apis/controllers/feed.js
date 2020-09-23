9ebfcc2a480eb839bbc90e9df71ba0d85696691f
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-23 13:11:49 -0400

    set up serverside validation

--- rest-apis/controllers/feed.js
+++ rest-apis/controllers/feed.js
@@ -1,3 +1,5 @@
+const {validationResult} = require('express-validator');
+
 exports.getPosts = (req, res) => {
     res.status(200).json({
         posts: [{
@@ -13,9 +15,16 @@ exports.getPosts = (req, res) => {
 }
 
 exports.createPost = (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            message: 'validation failed, entered data is incorrect.',
+            errors: errors.array()
+        })
+    }
+
     const title = req.body.title;
     const content = req.body.content;
-    console.log(title, content)
     //Create post in db
     res.status(201).json({
         message: 'Post created successfully',
