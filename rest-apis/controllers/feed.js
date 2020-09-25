99da099a09a6ff20d4eb36099d3d2c1e2e5467ac
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-25 17:20:51 -0400

    add pagination for posts

--- rest-apis/controllers/feed.js
+++ rest-apis/controllers/feed.js
@@ -7,11 +7,21 @@ const Post = require('../models/post');
 const { clearScreenDown } = require('readline');
 
 exports.getPosts = (req, res, next) => {
-    Post.find()
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+    .then(count => {
+        totalItems = count
+        return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
         res.status(200).json({
             message: 'Posts Fetched',
-            posts: posts
+            posts: posts,
+            totalItems: totalItems
         })
     })
     .catch(err => {
