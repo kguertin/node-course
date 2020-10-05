5808730371da86f8ecd0b59d841175c1af365925
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-05 17:26:40 -0400

    use client side to sign up using graphql

--- rest-apis/api_client/src/pages/Feed/Feed.js
+++ rest-apis/api_client/src/pages/Feed/Feed.js
@@ -40,7 +40,6 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket('http://localhost:8080');
   }
 
   loadPosts = direction => {
