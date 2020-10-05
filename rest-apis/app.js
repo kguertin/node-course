4a91513e39a8977e1d30ef01cdff68053861c50c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-05 16:29:55 -0400

    create user via graphql

--- rest-apis/app.js
+++ rest-apis/app.js
@@ -49,7 +49,8 @@ app.use((req, res, next) => {
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: graphqlResolver
+    rootValue: graphqlResolver,
+    graphiql: true
 }))
 
 app.use((error, req, res, next) => {
--- rest-apis/graphql/resolvers.js
+++ rest-apis/graphql/resolvers.js
@@ -1,8 +1,28 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user')
+
 module.exports = {
-    hello() {
+    createUser: async function ({ userInput }, req) {
+        // const email = userInput.email
+        const existingUser = await User.findOne({email: userInput.email});
+
+        if(existingUser){
+            const error = new Error('User already exists.');
+            throw error;
+        }
+
+        const hashedPass = await bcrypt.hash(userInput.password, 12);
+        const user = new User({
+            email: userInput.email,
+            name: userInput.name,
+            password: hashedPass
+        });
+
+        const createdUser = await user.save();
         return {
-            text: 'Hello World!',
-            views: 1234
+            ...createdUser._doc,
+            _id: createdUser._id.toString()
         }
     }
 }
