7b61f762018e29b0a034b176c3bcd7199027b84c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-07 21:23:00 -0400

    can delete user posts

--- rest-apis/app.js
+++ rest-apis/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +10,7 @@ const { v4: uuidv4 } =require('uuid')
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const MONGODB_URI = 'mongodb+srv://kevin:node1234@cluster0-kmmuu.mongodb.net/messages';
 
@@ -110,8 +110,3 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
-const clearImage = filePath => {
-  filePath = path.join(__dirname, filePath);
-  fs.unlink(filePath, err => console.log(err));
-};
