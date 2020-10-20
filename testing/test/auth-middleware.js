ba379da26804ddaca42ed8d8ddb9f83ff740e4ba
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-20 11:22:42 -0400

    write test for checking we get a properly formatted header and user describe to group tests

--- testing/test/auth-middleware.js
+++ testing/test/auth-middleware.js
@@ -2,12 +2,24 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('Should throw an error if not autherization header is present.', function() {
-    const req = {
-        get: function() {
-            return null;
+describe('Auth Middleware', function() {
+    it('Should throw an error if not autherization header is present.', function() {
+        const req = {
+            get: function() {
+                return null;
+            }
         }
-    }
-
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated');
+    
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated');
+    });
+    
+    it('should throw an error if the autherization header is only one string', function() {
+        const req = {
+            get: function() {
+                return 'xyz';
+            }
+        }
+    
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw;
+    })
 })
