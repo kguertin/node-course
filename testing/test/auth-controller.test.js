96a154d433494fb06d5ec658d8ee1e17b889d3c1
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-20 14:04:11 -0400

    create test db and run tests on returning correct user status

--- testing/test/auth-controller.test.js
+++ testing/test/auth-controller.test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const mongoose = require('mongoose')
 
 const User = require('../models/user');
 const authController = require('../controllers/auth');
@@ -23,6 +24,43 @@ describe('Auth Controller', () => {
         })
         User.findOne.restore();
     })
+    
+    it('Should send a response with a valid user status for an existing user', done => {
+        mongoose
+            .connect('mongodb+srv://kevin:node1234@cluster0-kmmuu.mongodb.net/test-messages')
+            .then(result =>{
+                const user = new User({
+                    email: 'test@test.com',
+                    password: 'tester',
+                    name: 'Test',
+                    posts: [],
+                    _id: '5c0f66b979af55031b34728a'
+                });
+                return user.save();
+            })
+            .then(() => {
+                const req = {
+                    userId: '5c0f66b979af55031b34728a'
+                }
+                const res = {
+                    statusCode: 500,
+                    userStatus: null,
+                    status: function(code) {
+                        this.statusCode = code;
+                        return this;
+                    },
+                    json: function(data) {
+                        this.userStatus = data.status;
+                    }
+                }
+                authController.getUserStatus(req, res, () => {}).then(() => {
+                    expect(res.statusCode).to.be.equal(200);
+                    expect(res.userStatus).to.be.equal('I am new!')
+                    done();
+                })
+            }) 
+            .catch(err => console.log(err));
 
+    })
 })
 
