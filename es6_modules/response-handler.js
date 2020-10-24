d5df9d72675ebd64a22c09b9bad893bbe3dc13b2
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-24 16:58:40 -0400

    build work around for __dirname and path using module type

--- es6_modules/response-handler.js
+++ es6_modules/response-handler.js
@@ -1,9 +1,15 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath }  from 'url';
+
+const __filename =fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 
 const resHandler = (req, res, next) => {
     fs.readFile('home.html', 'utf8', (err, data) => {
         res.send(data);
     })
+    res.sendFile(path.join(__dirname, 'home.html'));
 }
 
 export default resHandler;
