4a07aa51ca8aa5080eb187d2087b8125e7dbace6
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-24 22:04:31 -0400

    use node core module with promises

--- es6_modules/response-handler.js
+++ es6_modules/response-handler.js
@@ -1,4 +1,5 @@
-import fs from 'fs';
+// const fs = require(fs).promises
+import fs from 'fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath }  from 'url';
 
@@ -6,10 +7,12 @@ const __filename =fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
 
 const resHandler = (req, res, next) => {
-    fs.readFile('home.html', 'utf8', (err, data) => {
+    fs.readFile('home.html', 'utf8')
+    .then((data) => {
         res.send(data);
     })
-    res.sendFile(path.join(__dirname, 'home.html'));
+    .catch(err => console.log(err))
+    // res.sendFile(path.join(__dirname, 'home.html'));
 }
 
 export default resHandler;
