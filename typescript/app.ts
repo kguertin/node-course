943f19da72b4a28fdbb6eca121f4422caa878a34
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-27 18:18:44 -0400

    implement aliases and generics

--- typescript/app.ts
+++ typescript/app.ts
@@ -2,10 +2,18 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-const numResults: number[] = [];
+const numResults: Array<number> = [];
 const textResults: string[] = [];
 
-function add(num1: number | string, num2: number | string) {
+type NumOrString = number | string;
+type Result = { val: number; timestamp: Date };
+
+interface resultObj {
+    val: number,
+    timestamp: Date
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
     if(typeof num1 === 'number' && typeof num2 === 'number'){
         return num1 + num2;
     } else if(typeof num1 === 'string' && typeof num2 === 'string'){
@@ -14,7 +22,7 @@ function add(num1: number | string, num2: number | string) {
     return +num1 + +num2
 }
 
-function printResult(resultObj: { val: number; timestamp: Date }){
+function printResult(resultObj: Result){
     console.log(resultObj.val);
 }
 
@@ -30,3 +38,11 @@ buttonElement.addEventListener('click', () => {
     printResult({ val: result as number, timestamp: new Date() });
     console.log(numResults, textResults);
 })
+
+const myPromise = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It Worked!')
+    }, 1000)
+});
+
+myPromise.then(result => console.log(result.split('W')));
