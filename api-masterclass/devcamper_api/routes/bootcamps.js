280725c91a08c1fb7bb98b2192fbcb6da715be9f
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-02 12:05:27 -0500

    create controller and refactor starting routes

--- api-masterclass/devcamper_api/routes/bootcamps.js
+++ api-masterclass/devcamper_api/routes/bootcamps.js
@@ -1,30 +1,20 @@
 const express = require('express');
+const {
+  getBootcamps,
+  getBootcamp,
+  updateBootcamp,
+  createBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
-});
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-router.get('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, message: 'Create new bootcamp' });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `delete bootcamp ${req.params.id}` });
-});
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
