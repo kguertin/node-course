25e9842992bc51dd0f2c26407fe587753ddfd91a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-03 11:50:00 -0500

    add some filtering to get all bootcamps with mongoose expressions

--- api-masterclass/devcamper_api/controllers/bootcamps.js
+++ api-masterclass/devcamper_api/controllers/bootcamps.js
@@ -7,7 +7,16 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let querystr = JSON.stringify(req.query);
+  querystr = querystr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = Bootcamp.find(JSON.parse(querystr));
+
+  const bootcamps = await query;
 
   res.status(200).json({
     success: true,
