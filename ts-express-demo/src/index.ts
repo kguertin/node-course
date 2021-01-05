75c6f1b7fd856ba3d979d12ddec614a1d0930a35
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-01-04 21:35:06 -0500

    make login route and handle post request

--- ts-express-demo/src/index.ts
+++ ts-express-demo/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import bodyparser from 'body-parser';
+
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
