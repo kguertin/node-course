883139d292c84a162abf5f47822676816c128d4c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-15 19:34:58 -0500

    save original file name during image upload[

--- aws_s3/server-upload.js
+++ aws_s3/server-upload.js
@@ -2,7 +2,16 @@ const express = require('express');
 const multer = require('multer');
 const uuid = require('uuid').v4;
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, originalname);
+  },
+});
+const upload = multer({ storage });
 
 const app = express();
 app.use(express.static('public'));
