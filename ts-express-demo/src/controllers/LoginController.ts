3acc58029b7a7ab1e70f5434eded611ff1a4af02
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-05-01 15:36:42 -0400

    add decorators

--- ts-express-demo/src/controllers/LoginController.ts
+++ ts-express-demo/src/controllers/LoginController.ts
@@ -3,7 +3,7 @@ import { get } from './decorators/routes';
 import { controller } from './decorators/controller';
 
 @controller('/auth')
-class LoginController {
+export class LoginController {
   @get('/login')
   getLogin(req: Request, res: Response): void {
     res.send(`
--- ts-express-demo/src/index.ts
+++ ts-express-demo/src/index.ts
@@ -3,6 +3,8 @@ import bodyparser from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { router } from './routes/loginRoutes';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
 
 const app = express();
 
@@ -10,6 +12,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asecret'] }));
 
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
