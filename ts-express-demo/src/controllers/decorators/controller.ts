83b7818dbe7c9d4c722d305cabf7d492cbc7d168
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-05-07 15:49:09 -0400

    create validator middleware

--- ts-express-demo/src/controllers/decorators/controller.ts
+++ ts-express-demo/src/controllers/decorators/controller.ts
@@ -5,7 +5,21 @@ import { MetadataKeys } from './MetadataKeys';
 import { NextFunction, RequestHandler, Request, Response } from 'express';
 
 function bodyValidators(keys: string): RequestHandler {
-  return function (req: Request, res: Response, next: NextFunction) {};
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid Request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send('Invalid Request');
+        return;
+      }
+    }
+
+    next();
+  };
 }
 
 export function controller(routePrefix: string) {
@@ -29,8 +43,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.middlewear, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
