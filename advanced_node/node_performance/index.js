3912601c27767ce746f1a1d5c028f8f20c911c9d
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-10-26 19:44:00 -0400

    add crypto to simulate work when hitting route

--- advanced_node/node_performance/index.js
+++ advanced_node/node_performance/index.js
@@ -1,23 +1,18 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+
 const cluster = require('cluster');
-const { get } = require('http');
+const crypto = require('crypto')
 
 if (cluster.isMaster) {
   cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
 } else {
   const express = require('express');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b',100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    })
   });
 
   app.get('/fast', (req, res) => {
